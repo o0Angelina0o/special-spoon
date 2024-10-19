621b9b51c5a06c09dd640f4096c7209d796012a8
o0Angelina0o <0.angela24086.0@gmail.com>
2024-10-19 22:23:44 +0300

    Initial commit

---
+++ src/scripts/script.js
@@ -0,0 +1,31 @@
+
+
+const modal = document.querySelector('.modal')
+
+let isModalOpen = false
+
+function toggleModal(){
+  if(isModalOpen == true){
+    modal.style.display = 'none'
+    isModalOpen = false
+  } 
+  else {
+    modal.style.display = 'flex'
+    isModalOpen = true
+  }
+}
+
+const form = document.querySelector('.modal__form')
+
+
+form.addEventListener('submit', (event)=>{
+  event.preventDefault()
+  const inputValues = Array.from(document.querySelectorAll('.modal__form-input')).map((input)=>{
+    return input.value
+  })
+
+  console.log(inputValues)
+  form.reset()
+
+})
+
